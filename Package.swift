4e4290bc10aa8537b7b87b8ad1d6fd07698e157a
Tyler Helmrich <tylerhelmrich@tylers-air.lan>
2022-09-16 00:43:06 -0400

    trying again

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NXSDK",
-            targets: ["NXSDK"]),
+            targets: ["NXSDKTargets"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,6 +22,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "NXSDKTargets",
+            dependencies: [
+                .product(name: "NordicDFU", package: "IOS-DFU-Library")
+            ]
+        ),
         .binaryTarget(
           name: "NXSDK",
           path: "./Sources/NXSDK.xcframework")
---
+++ Sources/NXSDKTargets/NXSDKTargets.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tyler Helmrich on 9/16/22.
+//
+
+import Foundation
+//this is a dummy class necessary to get NXSDKTargets to work correctly
